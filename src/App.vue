531cf75e6dec8028927892014ce3ec784f563f90
masvidal778 <nuria.masvidal@gmail.com>
2023-08-21 17:47:48 +0200

    creating first component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-
+  <Counter></Counter>
 </template>
 
 <script>
 
+import Counter from "@/components/Counter.vue";
 
 export default {
   name: 'App',
+  components: {Counter},
 
 }
 </script>
--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,10 +1,14 @@
-<script setup>
+<template>
+  <h2>Counter</h2>
+  <p>10</p>
+</template>
 
+<script>
+export default {
+//  name: 'FirstCompoment'
+}
 </script>
 
-<template>
-
-</template>
 
 <style scoped>
 
