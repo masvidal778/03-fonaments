4b45dfc1f9d17868857db566210ed29a681eec62
masvidal778 <nuria.masvidal@gmail.com>
2023-08-21 18:49:41 +0200

    using properties in my first component

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,11 +1,17 @@
 <template>
   <h2>Counter</h2>
-  <p>10</p>
+  <p>{{ counter }} <sup>2</sup> =  {{ counter * counter}}</p>
 </template>
 
 <script>
 export default {
 //  name: 'FirstCompoment'
+  data() {
+    return{
+      //propiety with a value
+      counter: 5
+    }
+  }
 }
 </script>
 
