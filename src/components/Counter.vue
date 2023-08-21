8f71d32a1367e772b8c964ab74343f79f388240d
masvidal778 <nuria.masvidal@gmail.com>
2023-08-21 19:34:48 +0200

    using computed properties in my first component

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,6 +1,14 @@
 <template>
   <h2>Counter</h2>
-  <p>{{ counter }} <sup>2</sup> =  {{ counter * counter}}</p>
+  <p>{{ counter }} <sup>2</sup> =  {{ counter * counter }}</p>
+
+  <p>{{ counter }} <sup>2</sup> =  {{ squareCounter }}</p>
+  <p>{{ counter }} <sup>2</sup> =  {{ squareCounter }}</p>
+  <p>{{ counter }} <sup>2</sup> =  {{ squareCounter }}</p>
+  <p>{{ counter }} <sup>2</sup> =  {{ squareCounter }}</p>
+  <p>{{ counter }} <sup>2</sup> =  {{ squareCounter }}</p>
+  <p>{{ counter }} <sup>2</sup> =  {{ squareCounter }}</p>
+  <p>{{ counter }} <sup>2</sup> =  {{ squareCounter }}</p>
 </template>
 
 <script>
@@ -10,6 +18,19 @@ export default {
     return{
       //propiety with a value
       counter: 5
+    }
+  },
+  methods: {
+    getSquareValue() { //s'executarà cada cop que es faci la crida des del template
+      console.log('getSquareValue')
+      return this.counter * this.counter
+    }
+  },
+  computed: { //fan més eficient el procès, perquè guarden les propietats en el cache (només es cridarà un cop el mètode)
+    squareCounter() {
+      console.log('computed squareCounter')
+      //només es recalcula quan el counter canvia internament
+      return this.counter * this.counter
     }
   }
 }
