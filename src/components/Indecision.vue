4afc1d3b1e25015644073f4df5473bf334bd8f2a
masvidal778 <nuria.masvidal@gmail.com>
2023-08-27 13:24:25 +0200

    Using watch

--- src/components/Indecision.vue
+++ src/components/Indecision.vue
@@ -1,16 +1,86 @@
-<script setup>
-/*
+<script>
 export default {
+  data() {
+    return{
+      question: 'Hola món'
+    }
+  },
+  watch: {
+    question(value, oldValue){
+      if(!value.includes('?')) return
 
+      //TODO: Realizar petición HTTP
+    }
+  }
 }
 
- */
 </script>
 
 <template>
-<h1>Indecision</h1>
+  <img src="https://via.placeholder.com/250" alt="bg">
+  <div class="bg-dark"></div>
+
+  <div class="indecision-container">
+
+    <input
+        type="text"
+        placeholder="Fes una pregunta"
+        v-model="question"
+    >
+    <p>Recorda acabar amb un signe d'interrogació (?)</p>
+
+    <div>
+      <h2>{{ question }}</h2>
+      <h1>Sí, No, ... pensant</h1>
+    </div>
+
+  </div>
 </template>
 
 <style scoped>
+
+img, .bg-dark {
+  height: 100vh;
+  left: 0px;
+  max-height: 100%;
+  max-width: 100%;
+  position: fixed;
+  top: 0px;
+  width: 100vw;
+}
+
+.bg-dark {
+  background-color: rgba(0, 0, 0, 0.4);
+}
+
+.indecision-container {
+  position: relative;
+  z-index: 99;
+}
+
+input {
+  width: 250px;
+  padding: 10px 15px;
+  border-radius: 5px;
+  border: none;
+}
+
+input:focus {
+  outline: none;
+}
+
+p {
+  color: white;
+  font-size: 20px;
+  margin-top: 0px;
+}
+
+h1, h2 {
+  color: white;
+}
+
+h2 {
+  margin-top: 150px;
+}
 
 </style>
