62fff0707bb65f8e77f602aa7e94ecb645c92ad9
masvidal778 <nuria.masvidal@gmail.com>
2023-08-29 19:07:54 +0200

    Fent el primer test de Vue amb jest

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,21 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+//testsuite
+describe( 'Example Component', () => {
+  //dins d'un test suite es poden fer molts tests
+  test( 'Ha de ser més gran de 10', () => {
+
+    //Arrange (preparar pel test)
+    let value = 5;
+
+    //Act (provar)
+    value = value + 2;
+
+    //Assert (verificar)
+    if(value > 10){
+      //TODO tot bé
+    }else{
+      throw `${value} no és més gran de 10`
+    }
   })
-})
+
+} )
